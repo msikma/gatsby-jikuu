4c4c5ad1575cc967b454f9b6545c41a549a8b22e
Michiel Sikma <michiel@sikma.org>
2022-08-27 15:42:06 +0200

    Currently unused - keeping for later

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,13 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`
+    //   }
+    // },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
